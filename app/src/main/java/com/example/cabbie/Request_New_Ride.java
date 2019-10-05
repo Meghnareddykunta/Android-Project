156def91218c65263bdeae3f9ed33fc51c4168fd
Midhunkandakatla <midhunkandakatla143@gmail.com>
2019-10-04 23:22:18 -0500

    Created new activity_request__new__ride.xml

---
+++ app/src/main/java/com/example/cabbie/Request_New_Ride.java
@@ -0,0 +1,14 @@
+package com.example.cabbie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Request_New_Ride extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request__new__ride);
+    }
+}
