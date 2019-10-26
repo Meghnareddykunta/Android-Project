0ae8763c40408b495e74cf89a78901da27e8e6ca
ramesh nutulapathi <s534190@nwmissouri.edu>
2019-10-25 20:05:25 -0500

    Working on Looking for a ride.

---
+++ app/src/main/java/com/example/cabbie/LookForRideActivity.java
@@ -0,0 +1,13 @@
+package com.example.cabbie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LookForRideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
